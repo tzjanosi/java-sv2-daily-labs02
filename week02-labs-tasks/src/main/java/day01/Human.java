e749dfc44ae1f2f3f31dd4cfb92169204cc04149
tibo <tzjanosi@gamma.ttk.pte.hu>
2021-10-18 17:14:06 +0200

    "Human" v2

--- week02-labs-tasks/src/main/java/day01/Human.java
+++ week02-labs-tasks/src/main/java/day01/Human.java
@@ -1,2 +1,28 @@
-package day01;public class Human {
+package day01;
+
+public class Human {
+    private String name;
+    private int age;
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
---
+++ week02-labs-tasks/src/main/java/day01/HumanMain.java
@@ -0,0 +1,8 @@
+package day01;
+
+public class HumanMain {
+    public static void main(String[] args) {
+        Human human=new Human("joe",26);
+    }
+
+}
