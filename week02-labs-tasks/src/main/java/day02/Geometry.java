33c29e4bad228a3578b481bed6bf13676202401a
tibo <tzjanosi@gamma.ttk.pte.hu>
2021-10-19 16:08:33 +0200

    "Geometry" v2 Done

--- week02-labs-tasks/src/main/java/day02/Geometry.java
+++ week02-labs-tasks/src/main/java/day02/Geometry.java
@@ -6,7 +6,13 @@ public class Geometry {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Rectangle rectangle=new Rectangle(5.3,10.2);
+        System.out.println("Type in length of the first side:");
+        double side1 = scanner.nextDouble();
+        System.out.println("Type in length of the second side:");
+        double side2 = scanner.nextDouble();
+
+
+        Rectangle rectangle=new Rectangle(side1,side2);
         System.out.println("Területe:"+rectangle.calculateArea());
 
     }
